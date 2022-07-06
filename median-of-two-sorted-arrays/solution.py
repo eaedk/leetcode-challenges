d2abd003b969275b38165a10a1e00629db697efc
E K <emmanuelkoupoh@gmail.com>
2022-07-06 20:41:19 +0000

    p

--- median-of-two-sorted-arrays/solution.py
+++ median-of-two-sorted-arrays/solution.py
@@ -4,5 +4,3 @@ from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         #
-        print("findMedianSortedArrays")
-        return True
